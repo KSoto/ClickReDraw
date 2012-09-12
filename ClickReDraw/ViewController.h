210bd24663557bbfe4a9ceddcf37f88e6245ce75
knicholas <knicholas@cs101-5c.acad.fullerton.edu>
2012-09-12 16:22:01 -0700

    Initial Commit

---
+++ ClickReDraw/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ClickReDraw
+//
+//  Created by CS User on 9/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ClickReDrawTests/ClickReDrawTests.h
@@ -0,0 +1,13 @@
+//
+//  ClickReDrawTests.h
+//  ClickReDrawTests
+//
+//  Created by CS User on 9/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ClickReDrawTests : SenTestCase
+
+@end
